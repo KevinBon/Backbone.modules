782987e2610045ec9109301e46dbc6f81c4d3979
Kevin BON <KevinBon@users.noreply.github.com>
2017-09-11 13:00:41 -0400

    Update Navigate.js

--- Model/Navigate.js
+++ Model/Navigate.js
@@ -4,19 +4,23 @@
     Backbone.Abstract = Backbone.Abstract || {};
     Backbone.Abstract.Model = Backbone.Abstract.Model || {};
     Backbone.Abstract.Model.Navigate = {
-         /**
+        /**
          * Find the previous model in the linked collection
          * @method previous
          * @param  {Object|Function} where [description]
+         * @param  {Boolean} loop [description]
          * @return {?Backbone.Model}       [description]
          */
-        previous: function previous(where) {
+        previous: function previous(where, loop) {
+            loop = Boolean(loop);
             if (!this.collection)
                 return null;
-
+            function result(model) {
+                return model || (loop ? this.next(where) : null);
+            }
             var index = this.collection.indexOf(this);
             if (index === -1 || index === 0)
-                return null;
+                return result.call(this, null);
             if (where) {
                 var foundIndex = -1;
                 for (var i = 0; i < index; i++)
@@ -27,25 +31,29 @@
 
                 index = foundIndex;
                 if (index === -1)
-                    return null;
+                    return result.call(this, null);
             } else {
                 index -= 1;
             }
-            return this.collection.at(index) || null;
+            return result.call(this, this.collection.at(index));
         },
         /**
          * Find the next model in the linked collection
          * @method next
          * @param  {Object|Function} where [description]
+         * @param  {Boolean} loop [description]
          * @return {?Backbone.Model}       [description]
          */
-        next: function next(where) {
+        next: function next(where, loop) {
+            loop = Boolean(loop);
             if (!this.collection)
                 return null;
-
+            function result(model) {
+                return model || (loop ? this.previous(where) : null);
+            }
             var index = this.collection.indexOf(this);
             if (index === -1 || index === (this.collection.length - 1))
-                return null;
+                return result.call(this, null);
             if (where) {
                 var foundIndex = -1;
                 for (var i = index + 1; i < this.collection.length; i++)
@@ -56,11 +64,11 @@
 
                 index = foundIndex;
                 if (index === -1)
-                    return null;
+                    return result.call(this, null);
             } else {
                 index += 1;
             }
-            return this.collection.at(index) || null;
+            return result.call(this, this.collection.at(index));
         }
     };
 
